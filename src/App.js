eac015fea45d76f5bfa18bc6d7501259173ae1c8
HanaHww2 <hanahww2@gmail.com>
2022-01-11 23:30:54 +0900

    feat: react app 시작
    메타코딩님의 유튜브 강의
    (https://www.youtube.com/playlist?list=PL93mKxaRDidEfLM0I_FFb-98vfAQgXT82)를
    들으며 정리해본다.
    향후 리액트-스프링부트 토이 프로젝트를 진행하는 것을 목표로 한다.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+let a = 10; // 변수
+const b = 20; // 상수
+
+const myStyle = {
+  color: "red"
+};
+// JSX 문법
 function App() {
+  let c; // undefined;
+  console.log(1, c);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <div style={myStyle}>안녕 {a === 10 ? "10입니다." : "10이 아닙니다."}</div>
+    <h1 className='box-style'>heading tag, {b===20 && "20입니다."}</h1>
+    <hr/>
+  </div>
   );
 }
 
