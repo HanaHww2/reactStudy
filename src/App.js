aa8a702992d6c23878f36bec81e9799dd92601f5
HanaHww2 <hanahww2@gmail.com>
2022-01-13 23:11:11 +0900

    feat: 깊은 복사를 제공하는 함수
    concat, filter, map, sllice, spread(전개) 연산자의
    간단한 예제 코드를 확인해본다.
    위 메소드들은 원본 객체를 복제하여 활용함으로써
    원본의 불변성을 유지해준다.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
-let a = 10; // 변수
-const b = 20; // 상수
-
-const myStyle = {
-  color: "red"
-};
-// JSX 문법
 function App() {
-  let c; // undefined;
-  console.log(1, c);
+  let list = [1, 2, 3];
+
   return (
   <div>
-    <div style={myStyle}>안녕 {a === 10 ? "10입니다." : "10이 아닙니다."}</div>
-    <h1 className='box-style'>heading tag, {b===20 && "20입니다."}</h1>
-    <hr/>
+    <div>{list.map(n => <h1>{n}</h1>)}</div>
+    <div>{list.map(n => n)}</div>
   </div>
   );
 }
