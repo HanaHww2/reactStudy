5ecc5eb92bee42a147f54957ee042543ad4084e7
HanaHww2 <hanahww2@gmail.com>
2022-02-12 20:23:28 +0900

    feat: useState 동작 방식
    useState는 기존과는 다른 레퍼런스를 가진 상태값을 전달받을 때,
    re-rendering 동작을 수행한다.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React, { useState } from "react";
 import './App.css';
-import Sub from "./Sub";
 
 function App() {
-  // let number = 1; // 단순히 변수를 선언하면, React가 관리하는 상태값이 아니다.
-  // React 내의 hooks 라이브러리 활용
-  // -> hooks의 상태값으로 선언된다.
-  const[number, setNumber] = useState(1); 
 
-  const add = () => {
-    //number++; // 단순히 변수 값을 직접 변경
-    setNumber(number + 1); // 리액트에 number 값의 변경을 요청하는 함수
-    console.log("add", number);
-  };
+  console.log("App 실행됨")// rendering시 로그가 찍힌다.
+  
+  let sample = [
+    {id: 1, name:"홍길동"},
+    {id: 2, name:"임꺽정"},
+    {id: 3, name:"장보고"},
+    {id: 4, name:"하나"},
+  ];
+
+  const[users, setUsers] = useState([
+    {id: 1, name:"홍길동"},
+    {id: 2, name:"임꺽정"},
+    {id: 3, name:"장보고"},
+    {id: 4, name:"하나"},
+  ]); // useState(sample); 인 경우에는 download 클릭시 re-rendering이 일어나지 않는다.
+
+  const download = () => {
+    
+    sample.push({id: 5, name:"조자룡"}) // 데이터가 추가되었지만 레퍼런스 변경이 없으므로 re-rendering 수행하지 않음
+    console.log(sample);
+    const newSample = sample.concat({id: 6, name:"가나다"})
+    console.log(newSample);
+
+    // setUsers([...users, ...sample]); // 기존 데이터를 업데이트
+    // 새로운 데이터로 업데이트, 같은 데이터를 갖고 있지만 "레퍼런스"가 다르면 rerendering 수행
+    setUsers(newSample);
+    // setUsers([...sample, newData]); 스프레드 연산자를 사용하면 레퍼런스의 변경이 일어나므로 rerendering 수행
+  }
 
   // 상태값이 변경되면 렌더링(rebuild)이 발생
   // rebuild 시 하위의 모든 자식 컴포넌트들이 다시 생성될 수 있다.
   return (
   <div>
-    <h1>숫자 : {number}</h1>
-    <button onClick={add}>더하기</button>
-    <Sub></Sub>
+    <button onClick={download}>다운로드</button>
+    {users.map( u => (
+      <h1>
+        {u.id}. {u.name}
+      </h1>
+    ))}
   </div>
   );
 }
