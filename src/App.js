9a5d1fece29a32c4ad9ad08f8459b7b1d02993a2
HanaHww2 <hanahww2@gmail.com>
2022-02-13 00:03:51 +0900

    feat: useMemo 기본

--- src/App.js
+++ src/App.js
@@ -1,49 +1,43 @@
-import { useEffect, useState } from "react";
+import { useMemo, useState } from "react";
 import "./App.css";
-import Third from "./js/Third";
-import Sub, { num } from "./Sub";
 
 function App() {
-  const [data, setData] = useState(0);
-  const [search, setSearch] = useState(0);
-  const download = () => {
-    // 동적으로 데이터를 가져온다고 가정
-    let downloadData = 5;
-    setData(downloadData);
+  const [list, setList] = useState([1, 2, 3, 4]);
+  const [str, setStr] = useState("합계");
+
+  const getAddResult = () => {
+    let sum = 0;
+    list.forEach((i) => (sum += i));
+    console.log("sum 함수 실행됨:", sum);
+    return sum;
   };
 
-  // useEffect 실행시점 :
-  // (1) App() 함수가 최초 실행(rendering)될 때 (최초 mount)
-  // (2) 상태 변수가 변경될 때 (re-rendering) (dependencyList에 등록된 상태 변수)
-  useEffect(() => {
-    console.log("useEffect 샐행됨");
-    download();
-  }, [search]);
-  // dependencyList에 [] 빈 배열을 전달하면, (2)의 경우 무시 (어디에도 의존하지 않음)
-  // dependencyList를 [data] 로 두면 (2)의 경우에도 useEffect가 동작
+  const addResult = useMemo(() => getAddResult(), [list]); // getAddResult() 함수를 기억해두고, list가 변경되는 경우 실행한다.
 
   return (
     <div>
       <button
         onClick={() => {
-          setSearch(2);
-          // 정적으로 상태값을 변경했기 때문에, 한 번 클릭한 이후에는 더 클릭해도 상태값 변경이 없다.
-          // 버튼 클릭시, useEffect가 최초 한 번만 동작
+          setStr("안녕"); // 상태값 변경시 다른 요소들도 함께 re-rendering이 된다. -> useMemo로 최적화
         }}
       >
-        검색
+        문자 변경
       </button>
-      <h1>데이터 : {data}</h1>
       <button
         onClick={() => {
-          setData(data + 1);
+          setList([...list, 10]);
         }}
       >
-        더하기
+        리스트 값 추가
       </button>
-      <Sub />
-      <Third />
-      {num}
+      <div>
+        {list.map((i) => (
+          <h1>{i}</h1>
+        ))}
+      </div>
+      <div>
+        {str} : {addResult}
+      </div>
     </div>
   );
 }
