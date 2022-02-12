e0081dd3e33237d85b79cab0af66d82eec4d6f40
HanaHww2 <hanahww2@gmail.com>
2022-02-12 19:58:39 +0900

    feat:hooks의 useState 적용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
-import Wood from './Wood';
+import Sub from "./Sub";
 
 function App() {
+  // let number = 1; // 단순히 변수를 선언하면, React가 관리하는 상태값이 아니다.
+  // React 내의 hooks 라이브러리 활용
+  // -> hooks의 상태값으로 선언된다.
+  const[number, setNumber] = useState(1); 
+
+  const add = () => {
+    //number++; // 단순히 변수 값을 직접 변경
+    setNumber(number + 1); // 리액트에 number 값의 변경을 요청하는 함수
+    console.log("add", number);
+  };
+
+  // 상태값이 변경되면 렌더링(rebuild)이 발생
+  // rebuild 시 하위의 모든 자식 컴포넌트들이 다시 생성될 수 있다.
   return (
   <div>
-    <div>해</div>
-    <Wood />
+    <h1>숫자 : {number}</h1>
+    <button onClick={add}>더하기</button>
+    <Sub></Sub>
   </div>
   );
 }
---
+++ src/Sub.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Sub = () => {
+
+    // rebuild 수행의 분기점을 지정할 수 있는 함수가 존재
+    // 해당 컴포넌트의 상태값 변경이 없다면, 리빌드를 막을 수 있다. 
+    return (
+        <div>
+            <h1>Sub 입니다.</h1>
+        </div>
+    );
+};
+
+export default Sub;
