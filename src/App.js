b3aa1b026335c1050e374ec58963e8f9e9d66042
HanaHww2 <hanahww2@gmail.com>
2022-02-12 20:33:20 +0900

    refactor: useState 추가 활용

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
 
   console.log("App 실행됨")// rendering시 로그가 찍힌다.
-  
+
+  // let num = 5;
+  const [num, setNum] = useState(5);
   let sample = [
     {id: 1, name:"홍길동"},
     {id: 2, name:"임꺽정"},
@@ -21,15 +23,9 @@ function App() {
 
   const download = () => {
     
-    sample.push({id: 5, name:"조자룡"}) // 데이터가 추가되었지만 레퍼런스 변경이 없으므로 re-rendering 수행하지 않음
-    console.log(sample);
-    const newSample = sample.concat({id: 6, name:"가나다"})
-    console.log(newSample);
-
-    // setUsers([...users, ...sample]); // 기존 데이터를 업데이트
-    // 새로운 데이터로 업데이트, 같은 데이터를 갖고 있지만 "레퍼런스"가 다르면 rerendering 수행
-    setUsers(newSample);
-    // setUsers([...sample, newData]); 스프레드 연산자를 사용하면 레퍼런스의 변경이 일어나므로 rerendering 수행
+    setUsers([...sample, {id:num, name:"둘"}]); // sample 변수 또한 초기화되므로, 데이터는 항상 5개가 그려진다.
+    setNum(num + 1);
+    // num++; // re-rendering 발생시 모든 변수가 초기화되므로 이러한 방식으로는 동작하지 않는다.
   }
 
   // 상태값이 변경되면 렌더링(rebuild)이 발생
